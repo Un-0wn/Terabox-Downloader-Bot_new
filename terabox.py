490b4d1353cbae1f374222488970899768f530f2
D0st <th4park@protonmail.com>
2024-06-21 00:23:42 +0530

    modified:   config.env 	modified:   terabox.py

--- terabox.py
+++ terabox.py
@@ -30,8 +30,8 @@ bot_token = os.environ.get('BOT_TOKEN', '')
 if len(bot_token) == 0:
     logging.error("BOT_TOKEN variable is missing! Exiting now")
     exit(1)
-dump_id = int(-1002147965931)
-if dump_id == 0:
+dump_id = os.environ.get('DUMP_CHAT_ID', '')
+if len(dump_id) == 0:
     logging.error("DUMP_CHAT_ID variable is missing! Exiting now")
     exit(1)
 else:
